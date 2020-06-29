1574401a231af83ee956c51a8b6c227c3e5261a3
QuangPham <quangg2012@gmail.com>
2020-06-29 16:04:49 +0800

    Add files via upload

---
+++ scripts/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+PREFIX="--save_path results/ --batch_size 10 --data_path data/ --cuda yes --n_epochs 1 --use 1.0 --model bcl_dual --temperature 5 --memory_strength 100"
+CUDA_VISIBLE_DEVICES=0 python main.py $PREFIX --data_file mnist_permutations.pt --lr 0.03 --beta 0.3 --inner_steps 1 --n_meta 2
+CUDA_VISIBLE_DEVICES=0 python main.py $PREFIX --data_file cifar-cl.pt --lr 0.3 --beta 0.1 --inner_steps 2 --n_meta 1
+CUDA_VISIBLE_DEVICES=0 python main.py $PREFIX --data_file cub-cl.pt --lr 0.03 --beta 0.3 --inner_steps 1 --n_meta 2
+CUDA_VISIBLE_DEVICES=0 python main.py $PREFIX --data_file mini-cl.pt --lr 0.05 --beta 0.1 --inner_steps 2 --n_meta 1
+
+
